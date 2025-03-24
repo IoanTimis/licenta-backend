7008a0bda5f5ed07b5d73a6bcf235d79aa840236
IoanTimis <timisionut2000@gmail.com>
2025-03-24 21:39:59 +0200

    redirect_uri var

--- controllers/auth.js
+++ controllers/auth.js
@@ -114,7 +114,7 @@ const logout = (req, res) => {
 
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const REDIRECT_URI = 'https://licenta-backend-production.up.railway.app/auth/google/callback';
+const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
 
 const googleLogin = (req, res) => {
     const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=profile email`;
