5e316f9e363d12975884c4ae1f87b196d0d8ac60
IoanTimis <timisionut2000@gmail.com>
2025-03-26 19:52:20 +0200

    save progress

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,7 +25,6 @@ const checkSession = (req, res) => {
 
 const refreshAccessToken = (req, res) => {
   try {
-    console.log("Refresh token request:", req.cookies.refreshToken);
     const refreshToken = req.cookies.refreshToken;
 
     if (!refreshToken) {
@@ -33,8 +32,6 @@ const refreshAccessToken = (req, res) => {
     }
 
     const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
-
-    console.log("Decoded refresh token:", decoded);
 
     if (!decoded || !decoded.role) {
       return res.status(403).json({ error: "Invalid refresh token payload" });
