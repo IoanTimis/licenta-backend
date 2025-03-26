b968f5fc5fe8288ba4eb1c049054118b8d1871d9
IoanTimis <timisionut2000@gmail.com>
2025-03-26 19:36:58 +0200

    save progress

--- controllers/auth.js
+++ controllers/auth.js
@@ -92,6 +92,7 @@ const login = async (req, res) => {
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
+            sameSite: "None",
             maxAge: 30 * 24 * 60 * 60 * 1000, // 30 zile
         });
         
@@ -242,6 +243,7 @@ const googleCallback = async (req, res) => {
         res.cookie("refreshToken", refreshToken, {
           httpOnly: true,
           secure: process.env.NODE_ENV === "production",
+          sameSite: "None",
           maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
         });
 
